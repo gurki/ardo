c1653e5fad24369ecfbcbeb0ff8bc3d3403775a6
Tobias Gurdan <tobias@gurdan.de>
2014-10-15 20:00:58 +0200

    add vec3

--- code/src/Vector.h
+++ code/src/Vector.h
@@ -88,12 +88,104 @@ typedef vec2<int> vec2i;
 typedef vec2<float> vec2f;
 
 
-
 ////////////////////////////////////////////////////////////////////////////////
 template<>
 struct hash<vec2i> {
     size_t operator()(vec2i const & v) const noexcept {
         return 51 * (51 + std::hash<int>()(v.x)) + std::hash<int>()(v.y);
+    }
+};
+
+
+////////////////////////////////////////////////////////////////////////////////
+template <class T>
+class vec3
+{
+public:
+    
+    vec3(const T x_ = 0, const T y_ = 0, const T z_ = 0) : x(x_), y(y_), z(z_) {};
+    
+    const T norm() const {
+        return sqrt(x * x + y * y + z * z);
+    }
+    
+    const bool operator == (const vec3<T>& v) const {
+        return x == v.x && y == v.y && z == v.z;
+    }
+    
+    const bool operator != (const vec3<T>& v) const {
+        return !(*this == v);
+    }
+    
+    const vec3<T> operator + (const vec3<T>& v) const {
+        return vec3<T>(x + v.x, y + v.y, z + v.z);
+    }
+    
+    template <typename S>
+    const vec3<T> operator - (const vec3<S>& v) const {
+        return vec3<T>(x - v.x, y - v.y, z - v.z);
+    }
+    
+    template <typename S>
+    vec3<T>& operator = (const vec3<S>& v) const 
+    {
+        x = v.x;
+        y = v.y;
+        z = v.z;
+
+        return *this;
+    }
+    
+    template <typename S>
+    const vec3<T> operator / (const S s) const {
+        return vec3<T>(x / s, y / s, z / s);
+    }
+    
+    friend vec3<T> operator * (const T s, const vec3<T>& v) {
+        return vec3<T>(s * v.x, s * v.y, s * v.z);
+    }
+    
+    friend vec3<T> operator * (const vec3<T>& v, const T s) {
+        return vec3<T>(v.x * s, v.y * s, v.z * s);
+    }
+    
+    friend T dot(const vec3<T>& v1, const vec3<T>& v2) {
+        return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+    }
+    
+    void operator += (const vec3<T>& v) {
+        x += v.x;
+        y += v.y;
+        z += v.z;
+    }
+    
+    void operator *= (const T s) {
+        x *= s;
+        y *= s;
+        z *= s;
+    }
+    
+    friend ostream& operator << (ostream& out, const vec3<T>& v) {
+        out << "(" << v.x << ", " << v.y << ", " << v.z << ")";
+        return out;
+    }
+    
+    T x;
+    T y;
+    T z;
+};
+
+
+////////////////////////////////////////////////////////////////////////////////
+typedef vec3<int> vec3i;
+typedef vec3<float> vec3f;
+
+
+////////////////////////////////////////////////////////////////////////////////
+template<>
+struct hash<vec3i> {
+    size_t operator()(vec3i const & v) const noexcept {
+        return 51 * (51 * (51 + std::hash<int>()(v.x)) + std::hash<int>()(v.y)) + std::hash<int>()(v.z);
     }
 };
 
