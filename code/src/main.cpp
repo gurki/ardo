fe8c3dc76b1fa76619de6c51b1cf81bdf68404d5
Tobias Gurdan <tobias@gurdan.de>
2014-10-16 04:05:18 +0200

    massive reverb, holy crap

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -42,7 +42,7 @@ int main(int argc, char* argv[])
     
     result_ = system_->setOutput(FMOD_OUTPUTTYPE_AUTODETECT); FMOD::check(result_);
     result_ = system_->setSpeakerMode(FMOD_SPEAKERMODE_STEREO); FMOD::check(result_);
-    result_ = system_->init(64, FMOD_INIT_NORMAL | FMOD_INIT_3D_RIGHTHANDED, 0); FMOD::check(result_);
+    result_ = system_->init(64, FMOD_3D | FMOD_INIT_NORMAL | FMOD_INIT_3D_RIGHTHANDED, 0); FMOD::check(result_);
 
     //  create sound
     FMOD_CREATESOUNDEXINFO exinfo;
@@ -55,17 +55,34 @@ int main(int argc, char* argv[])
     exinfo.length = exinfo.defaultfrequency * sizeof(float) * exinfo.numchannels * 2.0f;
     
     FMOD::Sound* sound = 0;
-    result_ = system_->createSound(0, FMOD_LOOP_NORMAL | FMOD_SOFTWARE | FMOD_OPENUSER, &exinfo, &sound); FMOD::check(result_);
+    result_ = system_->createSound(0, FMOD_3D | FMOD_LOOP_NORMAL | FMOD_SOFTWARE | FMOD_OPENUSER, &exinfo, &sound); FMOD::check(result_);
 
     //  record and play
     FMOD::Channel* channel = 0;
     system_->playSound(FMOD_CHANNEL_FREE, sound, false, &channel);
     system_->recordStart(0, sound, true);
+    channel->setSpeakerMix(5, 5, 5, 5, 5, 5, 5, 5);
+
+    FMOD_VECTOR listenerpos = { 0.0f, 0.0f, 0.0f };
+    system_->set3DListenerAttributes(0, &listenerpos, 0, 0, 0);
     
+    //  add massive reverb
+    FMOD::Reverb* reverb;
+    system_->createReverb(&reverb);
+    reverb->setActive(true);
+    
+    FMOD_VECTOR pos = { 0.0f, 0.0f, 0.0f };
+    float mindist = 0.0f;
+    float maxdist = 100.0f;
+    reverb->set3DAttributes(&pos, mindist, maxdist);
+
+    FMOD_REVERB_PROPERTIES properties;
+    properties.DecayTime = 100;
+    properties.Reverb = 100;
+    reverb->setProperties(&properties);
+    
+    //  loop
     sf::Clock clock;
-    int rec = 0;
-    unsigned int start = 0;
-    unsigned int end = 0;
     
     while (true)
     {
@@ -73,50 +90,9 @@ int main(int argc, char* argv[])
         
         unsigned int position;
         system_->getRecordPosition(0, &position);
+        channel->setPosition(position, FMOD_TIMEUNIT_PCM);
+
         
-        if (rec < 2) {
-            channel->setPosition(position, FMOD_TIMEUNIT_PCM);
-        }
-        
-        //  compute mean amplitude
-        const int nsamples = 1000;
-        const int thresh = -4;
-        
-        float data[nsamples];
-        channel->getWaveData(data, nsamples, 0);
-        
-        float max = -numeric_limits<float>::max();
-        float min = numeric_limits<float>::max();
-        
-        for (int i = 0; i < nsamples; i++) {
-            max = data[i] > max ? data[i] : max;
-            min = data[i] < min ? data[i] : min;
-        }
-        
-        float amp = log(max - min);
-        
-        if (rec == 0 && amp > thresh)
-        {
-            cout << position << endl;
-//            system_->recordStop(0);
-//            system_->recordStart(0, sound1, true);
-            start = position;
-            rec = 1;
-            
-            cout << clock.getElapsedTime().asSeconds() << ": start" << endl;
-        }
-        else if (rec == 1 && amp <= thresh)
-        {
-            end = position;
-            cout<< position << endl;
-            
-            system_->recordStop(0);
-            channel->setLoopPoints(start, FMOD_TIMEUNIT_PCM, end, FMOD_TIMEUNIT_PCM);
-            channel->setLoopCount(-1);
-            rec = 2;
-            
-            cout << clock.getElapsedTime().asSeconds() << ": stop" << endl;
-        }
         
         sf::sleep(sf::seconds(0.01f));
     }
