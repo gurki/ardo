25ea7483568df1d6d081bf643a6f5014db487aa9
Tobias Gurdan <tobias@gurdan.de>
2014-10-16 13:04:12 +0200

    start hud implementation

---
+++ code/src/Hud.cpp
@@ -0,0 +1,9 @@
+//
+//  Hud.cpp
+//  ordo
+//
+//  Created by Tobias Gurdan on 16/10/14.
+//  Copyright (c) 2014 Tobias Gurdan. All rights reserved.
+//
+
+#include "Hud.h"
---
+++ code/src/Hud.h
@@ -0,0 +1,14 @@
+//
+//  Hud.h
+//  ordo
+//
+//  Created by Tobias Gurdan on 16/10/14.
+//  Copyright (c) 2014 Tobias Gurdan. All rights reserved.
+//
+
+#ifndef __ordo__Hud__
+#define __ordo__Hud__
+
+#include <stdio.h>
+
+#endif /* defined(__ordo__Hud__) */
